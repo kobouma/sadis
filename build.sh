50fbf9bd0fd4793eceeae436ce9b60d74d5c643e
samurai226 <fidel.lesamurai@gmail.com>
2026-04-11 00:50:06 +0000

    fix: add DJANGO_SETTINGS_MODULE in build.sh

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ echo "==> Installation des dépendances"
 pip install -r requirements.txt
 
 echo "==> Collecte des fichiers statiques"
-python manage.py collectstatic --no-input
+DJANGO_SETTINGS_MODULE=config.settings.prod python manage.py collectstatic --no-input
 
 echo "==> Migrations"
-python manage.py migrate --no-input
+DJANGO_SETTINGS_MODULE=config.settings.prod python manage.py migrate --no-input
 
 echo "==> Build terminé ✅"
